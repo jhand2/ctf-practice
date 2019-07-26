9e279dd9da8761521727063b875f69c657834d09
Jordan Hand <jordanhand22@gmail.com>
2019-07-26 16:18:16 -0700

    Save progress on 5

--- 5/solution.py
+++ 5/solution.py
@@ -1,1 +1,16 @@
 import requests as r
+
+url = "https://drivetothetarget.web.ctfcompetition.com/"
+
+lat = 51.6498
+lon = 0.0982
+token = "gAAAAABdO4gjUEgzqOLjgDiogyAPX6xMqX2PdxLrZSi5G8C2Dp57DADOQr0mTzzCfuoEVbSxlUtoO9IM_Zo-mFB-oco9hofsReKkkoiPFgdJ5DpO5lf_cUKn6c3JoCUAwB9wdUKU-837"
+
+for i in range(3):
+    lon -= 0.001
+    full = url + "?lat=%s&lon=%s&token=%s" % (lat, lon, token)
+    res = r.get(full)
+    print(res.text)
+    print()
+
+
