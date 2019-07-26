79acaf6387ed5ba02316c638989c0f9df9c57144
Jordan Hand <jordanhand22@gmail.com>
2019-07-26 08:05:44 -0700

    Save state

--- 4/solution.py
+++ 4/solution.py
@@ -1,3 +1,6 @@
+from vm import *
+import re
+
 def is_mirrored(n):
     s = []
     while n > 0:
@@ -34,8 +37,70 @@ def is_prime(n):
     # then n is a prime number
     return True
 
+def decode_char(c, f):
+    if c in VM.OPERATIONS:
+        return VM.OPERATIONS[c].__name__
+    elif ord(c) >= ord('0') and ord(c) <= ord('9'):
+        for i in range(3): f.read(1)
+        return str(ord(c) - ord('0'))
+    else:
+        return c
 
-for i in range(1000):
-    if is_mirrored(i) and is_prime(i):
-        print(i)
+def replace_symbols():
+    s = ''
+    with open("program", "r") as f:
+        while True:
+            c = f.read(1)
+            if not c:
+                break
+            s = s + decode_char(c, f)
+    return s
 
+
+def get_next_mirrored_prime(idx, last_prime=None, last_idx=None):
+    i = 1
+    prime = 2
+    if last_prime is not None and last_idx is not None:
+        i = last_idx
+        prime = last_prime
+
+    while i < idx:
+        prime = prime + 1
+        if is_mirrored(prime):
+            if is_prime(prime):
+                i += 1
+
+    return prime
+
+
+def main():
+    # Get file and replace known symbols
+    s = replace_symbols()
+
+    with open("converted.txt", "w") as f:
+        f.write(s)
+
+    # Split instructions into sections
+    s = s.split('\n\n')
+
+    for section in s:
+        # Only look at sections that are loading values onto the stack
+        if section.startswith("load"):
+            print(section)
+            res = re.findall(r"[0-9]+", section)
+
+            # The starting stack
+            stack = res[:-1]
+
+            print(res)
+            print()
+
+    last_prime = None
+    last_idx = None
+    for i in range(1, 11):
+        p = get_next_mirrored_prime(i, last_prime, last_idx)
+        last_prime = p
+        last_idx = i
+        print(p)
+
+main()
